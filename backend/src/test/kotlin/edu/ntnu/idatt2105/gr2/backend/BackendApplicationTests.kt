4b993647b741823a14b9cd80af1f76589697988e
Henrik Kvamme <henrik.halvorsen.kvamme@gmail.com>
2025-03-30 19:42:51 +0200

    Configured mysql docker compose

--- backend/src/test/kotlin/edu/ntnu/idatt2105/gr2/backend/BackendApplicationTests.kt
+++ backend/src/test/kotlin/edu/ntnu/idatt2105/gr2/backend/BackendApplicationTests.kt
@@ -1,13 +1,14 @@
 package edu.ntnu.idatt2105.gr2.backend
 
+import edu.ntnu.idatt2105.gr2.backend.config.TestConfig
 import org.junit.jupiter.api.Test
 import org.springframework.boot.test.context.SpringBootTest
+import org.springframework.context.annotation.Import
 
 @SpringBootTest
+@Import(TestConfig::class)
 class BackendApplicationTests {
-
 	@Test
 	fun contextLoads() {
 	}
-
 }
---
+++ backend/src/test/kotlin/edu/ntnu/idatt2105/gr2/backend/config/TestConfig.kt
@@ -0,0 +1,27 @@
+package edu.ntnu.idatt2105.gr2.backend.config
+
+import edu.ntnu.idatt2105.gr2.backend.service.UserContextService
+import org.springframework.boot.test.context.TestConfiguration
+import org.springframework.context.annotation.Bean
+import org.springframework.context.annotation.Primary
+import org.springframework.jdbc.datasource.embedded.EmbeddedDatabaseBuilder
+import org.springframework.jdbc.datasource.embedded.EmbeddedDatabaseType
+import javax.sql.DataSource
+
+@TestConfiguration
+class TestConfig {
+    @Bean
+    fun dataSource(): DataSource {
+        return EmbeddedDatabaseBuilder()
+            .setType(EmbeddedDatabaseType.H2)
+            .build()
+    }
+
+    @Bean
+    @Primary
+    fun testUserContextService(): UserContextService {
+        val service = UserContextService()
+        service.setCurrentUsername("test-user")
+        return service
+    }
+}
