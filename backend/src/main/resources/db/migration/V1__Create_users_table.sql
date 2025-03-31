b69e0671df74e13af40b14785835f31300b44c00
Henrik Kvamme <henrik.halvorsen.kvamme@gmail.com>
2025-03-31 14:51:13 +0200

    Updated users table migration

--- backend/src/main/resources/db/migration/V1__Create_users_table.sql
+++ backend/src/main/resources/db/migration/V1__Create_users_table.sql
@@ -1,7 +1,9 @@
 CREATE TABLE users
 (
-    user_id   INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
-    password   VARCHAR(255) NOT NULL,
+    user_id    INT          NOT NULL AUTO_INCREMENT PRIMARY KEY,
     email      VARCHAR(255) NOT NULL,
+    name       VARCHAR(255) NOT NULL,
+    password   VARCHAR(255) NOT NULL,
+    is_admin   BOOLEAN      NOT NULL DEFAULT FALSE,
     created_at TIMESTAMP    NOT NULL DEFAULT CURRENT_TIMESTAMP
 );
