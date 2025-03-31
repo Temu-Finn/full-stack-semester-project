ed61990037107b3dc345a618cf857e5674929ae7
Henrik Utistog Hausberg <henrik.utistog.hausberg@outlook.com>
2025-03-31 15:32:25 +0200

    Added nav bar

--- frontend/src/views/AboutView.vue
+++ frontend/src/views/AboutView.vue
@@ -1,15 +1,19 @@
 <template>
-  <div class="about">
-    <h1>This is an about page</h1>
-  </div>
+  <main>
+    <div class="about">
+      <h1>This is an about page</h1>
+    </div>
+  </main>
 </template>
 
 <style>
 @media (min-width: 1024px) {
   .about {
-    min-height: 100vh;
+    min-height: 90vh;
     display: flex;
     align-items: center;
   }
 }
 </style>
+<script setup lang="ts">
+</script>
