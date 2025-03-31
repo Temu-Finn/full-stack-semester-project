f855fe39d651fc07afbd881bf9d5789203b82f74
Jonas Moe Bjørnbom <jonas@bjornbom.no>
2025-03-31 16:47:24 +0200

    created controller, service and repository classes for item

---
+++ backend/src/main/kotlin/edu/ntnu/idatt2105/gr2/backend/controller/ItemController.kt
@@ -0,0 +1,11 @@
+package edu.ntnu.idatt2105.gr2.backend.controller
+
+import org.springframework.web.bind.annotation.RestController
+import org.springframework.web.bind.annotation.RequestMapping
+
+@RestController
+@RequestMapping("/items")
+class ItemController {
+
+
+}
