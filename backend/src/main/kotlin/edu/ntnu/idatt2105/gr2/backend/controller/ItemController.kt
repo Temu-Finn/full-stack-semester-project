1cdc1585c3cba1b61af9c5b84946fdf79ef1f4f5
Jonas Moe Bjørnbom <jonas@bjornbom.no>
2025-04-01 14:48:20 +0200

    created first functions for ItemController

--- backend/src/main/kotlin/edu/ntnu/idatt2105/gr2/backend/controller/ItemController.kt
+++ backend/src/main/kotlin/edu/ntnu/idatt2105/gr2/backend/controller/ItemController.kt
@@ -1,11 +1,39 @@
 package edu.ntnu.idatt2105.gr2.backend.controller
 
+import edu.ntnu.idatt2105.gr2.backend.model.Item
+import edu.ntnu.idatt2105.gr2.backend.service.ItemService
+import org.springframework.http.HttpStatus
+import org.springframework.http.ResponseEntity
+import org.springframework.web.bind.annotation.DeleteMapping
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.PathVariable
+import org.springframework.web.bind.annotation.PostMapping
+import org.springframework.web.bind.annotation.RequestBody
 import org.springframework.web.bind.annotation.RestController
 import org.springframework.web.bind.annotation.RequestMapping
 
 @RestController
-@RequestMapping("/items")
-class ItemController {
+@RequestMapping("/api/items")
+class ItemController(private val itemService: ItemService) {
 
+    @PostMapping
+    fun createItem(@RequestBody item: Item): ResponseEntity<Item> {
+        val savedItem = itemService.createItem(item)
+        return ResponseEntity.status(HttpStatus.CREATED).body(savedItem)
+    }
 
+    @GetMapping("/category/{id}")
+    fun getItemsByCategoryId(@PathVariable id: Long): ResponseEntity<List<Item>> {
+        val items = itemService.getItemsByCategoryId(id)
+        return ResponseEntity.ok(items)
+    }
+
+    @DeleteMapping("/{id}")
+    fun deleteItemById(@PathVariable id: Long): ResponseEntity<Void> {
+        return if (itemService.deleteItemById(id)) {
+            ResponseEntity.noContent().build()
+        } else {
+            ResponseEntity.notFound().build()
+        }
+    }
 }
