6a1b25e2c4d90534697f93ee82f2c546bb362b70
Mathias Hølestøl <151520307+Mathole01@users.noreply.github.com>
2025-04-01 17:08:03 +0200

    Feat(categories)/server (#13)
    * add category functionality
    
    * Fix missing semicolon
    
    * Remove boilerplate code
    
    * Add transaction dependency
    
    * Bypass jwt security for testing
    
    * Add verification for updateDescription
    
    * Add method saveALl
    
    * Implement categoryService test class
    
    * Fixed migration name
    
    ---------
    
    Co-authored-by: Henrik Kvamme <henrik.halvorsen.kvamme@gmail.com>

---
+++ backend/src/main/resources/db/migration/V2__Create_category_table.sql
@@ -0,0 +1,7 @@
+CREATE TABLE categories
+(
+    category_id     INT             NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    name            VARCHAR(255)    NOT NULL UNIQUE,
+    description     TEXT            NOT NULL,
+    created_at      TIMESTAMP       NOT NULL DEFAULT CURRENT_TIMESTAMP
+);
--- backend/src/test/kotlin/edu/ntnu/idatt2105/gr2/backend/config/TestConfig.kt
+++ backend/src/test/kotlin/edu/ntnu/idatt2105/gr2/backend/config/TestConfig.kt
@@ -1,11 +1,18 @@
 package edu.ntnu.idatt2105.gr2.backend.config
 
+import edu.ntnu.idatt2105.gr2.backend.configs.JwtAuthenticationFilter
+import edu.ntnu.idatt2105.gr2.backend.model.User
 import edu.ntnu.idatt2105.gr2.backend.service.UserContextService
+import edu.ntnu.idatt2105.gr2.backend.service.JwtService
+import edu.ntnu.idatt2105.gr2.backend.service.AuthenticationService
+import org.mockito.Mockito
 import org.springframework.boot.test.context.TestConfiguration
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Primary
 import org.springframework.jdbc.datasource.embedded.EmbeddedDatabaseBuilder
 import org.springframework.jdbc.datasource.embedded.EmbeddedDatabaseType
+import org.mockito.Mockito.mock
+import org.mockito.Mockito.`when`
 import javax.sql.DataSource
 
 @TestConfiguration
@@ -23,5 +30,18 @@ class TestConfig {
         val service = UserContextService()
         service.setCurrentUserId(1)
         return service
+    }
+
+    @Bean
+    @Primary
+    fun jwtAuthenticationFilter(): JwtAuthenticationFilter {
+        return Mockito.mock(JwtAuthenticationFilter::class.java)
+    }
+
+    // Mocks authentication and authorization
+    @Bean
+    @Primary
+    fun jwtService(): JwtService {
+        return Mockito.mock(JwtService::class.java)
     }
 }
