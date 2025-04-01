625decdeab67038ea7087c90980f8d728fbe74b8
Jonas Moe Bjørnbom <jonas@bjornbom.no>
2025-04-01 10:45:30 +0200

    started work on itemRepository

--- backend/src/main/kotlin/edu/ntnu/idatt2105/gr2/backend/repository/ItemRepository.kt
+++ backend/src/main/kotlin/edu/ntnu/idatt2105/gr2/backend/repository/ItemRepository.kt
@@ -1,4 +1,8 @@
 package edu.ntnu.idatt2105.gr2.backend.repository
 
-class ItemRepository {
+import org.springframework.stereotype.Repository
+import javax.sql.DataSource
+
+@Repository
+class ItemRepository(private val dataSource: DataSource) {
 }
