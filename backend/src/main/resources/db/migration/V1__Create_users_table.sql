cdc3c8ed3a00298970e60869196338c87d2a9b8d
Henrik Kvamme <henrik.halvorsen.kvamme@gmail.com>
2025-04-01 21:46:05 +0200

    Changed to less redundant id naming

--- backend/src/main/resources/db/migration/V1__Create_users_table.sql
+++ backend/src/main/resources/db/migration/V1__Create_users_table.sql
@@ -1,6 +1,6 @@
 CREATE TABLE users
 (
-    user_id    INT          NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    id         INT          NOT NULL AUTO_INCREMENT PRIMARY KEY,
     email      VARCHAR(255) NOT NULL UNIQUE,
     name       VARCHAR(255) NOT NULL,
     password   VARCHAR(255) NOT NULL,
