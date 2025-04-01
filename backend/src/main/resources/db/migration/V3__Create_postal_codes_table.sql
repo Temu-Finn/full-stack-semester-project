58139d739d38716a00dc7c57ffe90987bd0c1141
Henrik Kvamme <henrik.halvorsen.kvamme@gmail.com>
2025-04-02 00:42:23 +0200

    Renamed postal_area to city

--- backend/src/main/resources/db/migration/V3__Create_postal_codes_table.sql
+++ backend/src/main/resources/db/migration/V3__Create_postal_codes_table.sql
@@ -1,7 +1,7 @@
 CREATE TABLE postal_codes
 (
     postal_code  VARCHAR(10)  NOT NULL PRIMARY KEY,
-    postal_area  VARCHAR(100) NOT NULL COMMENT 'The common name for the postal code area (e.g., city/town)',
+    city VARCHAR(100) NOT NULL COMMENT 'The postal code area (e.g., poststed)',
     municipality VARCHAR(100) NOT NULL,
     county       VARCHAR(100) NOT NULL
 );
