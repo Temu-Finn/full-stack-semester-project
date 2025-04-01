cb4553eebcec123c4aea4c6aff038b6c3ba20ae7
Henrik Kvamme <henrik.halvorsen.kvamme@gmail.com>
2025-04-01 21:48:53 +0200

    Limits string size for category name and description

--- backend/src/main/resources/db/migration/V2__Create_category_table.sql
+++ backend/src/main/resources/db/migration/V2__Create_category_table.sql
@@ -1,7 +1,7 @@
 CREATE TABLE categories
 (
     id          INT          NOT NULL AUTO_INCREMENT PRIMARY KEY,
-    name        VARCHAR(255) NOT NULL UNIQUE,
-    description TEXT         NOT NULL,
+    name        VARCHAR(30)  NOT NULL UNIQUE,
+    description VARCHAR(255) NOT NULL,
     created_at  TIMESTAMP    NOT NULL DEFAULT CURRENT_TIMESTAMP
 );
