e6395d37a29511dab144061978b0f2f16373a8e5
Jonas Moe Bjørnbom <jonas@bjornbom.no>
2025-04-01 13:45:58 +0200

    fixing indents

--- backend/src/main/kotlin/edu/ntnu/idatt2105/gr2/backend/model/Item.kt
+++ backend/src/main/kotlin/edu/ntnu/idatt2105/gr2/backend/model/Item.kt
@@ -1,12 +1,12 @@
 package edu.ntnu.idatt2105.gr2.backend.model
 
-    class Item (
-        val id: Long = -1,
-        val owner: Long,
-        val category: Long,
-        val name: String,
-        var price: Double,
-        var description: String,
-        var isSold: Boolean,
-        var condition: String
-    )
+class Item (
+    val id: Long = -1,
+    val owner: Long,
+    val category: Long,
+    val name: String,
+    var price: Double,
+    var description: String,
+    var isSold: Boolean,
+    var condition: String
+)
