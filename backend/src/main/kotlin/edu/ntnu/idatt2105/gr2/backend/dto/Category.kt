0ae4b95f8f1b29545b8e66413828d0a28290ceb0
Mathias Hølestøl <151520307+Mathole01@users.noreply.github.com>
2025-04-07 11:38:53 +0200

    BEFORE APPROVING: READ COMMENT --- Fixed tests not using testcontainer, added CategoryController (#40)
    * Fix tests not using testcontainer
    
    * Fix and rename .properties file
    
    * Add DTOs for category endpoint
    
    * Add id data field and refactoring
    
    * Add method to check if user is admin
    
    * Implement CategoryController

---
+++ backend/src/main/kotlin/edu/ntnu/idatt2105/gr2/backend/dto/Category.kt
@@ -0,0 +1,28 @@
+package edu.ntnu.idatt2105.gr2.backend.dto
+
+import jakarta.validation.constraints.Email
+import jakarta.validation.constraints.NotBlank
+import jakarta.validation.constraints.Size
+
+data class CategoryResponse(
+    val id: Int,
+    val name: String,
+    val description: String
+)
+
+data class CategoriesResponse(
+    val categories: List<CategoryResponse>
+)
+
+data class CreateCategoryRequest(
+    @field:NotBlank(message = "Name cannot be empty")
+    val name: String,
+
+    @field:NotBlank(message = "Description cannot be empty")
+    val description: String
+)
+
+data class DeleteCategoryRequest(
+    @field:NotBlank(message = "Name cannot be empty")
+    val name: String
+)
