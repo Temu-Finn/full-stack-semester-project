eb1c6f8dc20955e2e6890809164e0302681e3e07
Henrik Kvamme <henrik.halvorsen.kvamme@gmail.com>
2025-04-07 13:43:14 +0200

    Changed item_images table

--- backend/src/main/resources/db/migration/V11__Image_url_to_blob.sql
+++ backend/src/main/resources/db/migration/V11__Image_url_to_blob.sql
@@ -1,5 +1,7 @@
 ALTER TABLE item_images
-ADD COLUMN image_blob LONGBLOB;
+    ADD COLUMN image_data LONGBLOB;
+ALTER TABLE item_images
+    ADD COLUMN file_type VARCHAR(50);
 
 ALTER TABLE item_images
 DROP COLUMN image_url;
