44e5975d5ba9f3914f5e5955bed8a31635319793
Henrik Kvamme <henrik.halvorsen.kvamme@gmail.com>
2025-04-07 21:20:00 +0200

    Save image repo

--- backend/src/main/kotlin/edu/ntnu/idatt2105/gr2/backend/model/Image.kt
+++ backend/src/main/kotlin/edu/ntnu/idatt2105/gr2/backend/model/Image.kt
@@ -1,11 +1,10 @@
 package edu.ntnu.idatt2105.gr2.backend.model
 
 
-class Image (
+data class Image(
     val id: Int = -1,
     val data: ByteArray,
     val fileType: String,
-    val isPrimary: Boolean = false,
     val altText: String? = null,
 ) {
     init {
@@ -16,5 +15,27 @@ class Image (
     fun base64(): String {
         return java.util.Base64.getEncoder().encodeToString(data)
             .let { "data:$fileType;base64,$it" }
+    }
+
+    override fun equals(other: Any?): Boolean {
+        if (this === other) return true
+        if (javaClass != other?.javaClass) return false
+
+        other as Image
+
+        if (id != other.id) return false
+        if (!data.contentEquals(other.data)) return false
+        if (fileType != other.fileType) return false
+        if (altText != other.altText) return false
+
+        return true
+    }
+
+    override fun hashCode(): Int {
+        var result = id
+        result = 31 * result + data.contentHashCode()
+        result = 31 * result + fileType.hashCode()
+        result = 31 * result + (altText?.hashCode() ?: 0)
+        return result
     }
 }
--- backend/src/main/kotlin/edu/ntnu/idatt2105/gr2/backend/repository/ImageRepository.kt
+++ backend/src/main/kotlin/edu/ntnu/idatt2105/gr2/backend/repository/ImageRepository.kt
@@ -2,10 +2,31 @@ package edu.ntnu.idatt2105.gr2.backend.repository
 
 import edu.ntnu.idatt2105.gr2.backend.model.Image
 import org.springframework.stereotype.Repository
+import javax.sql.DataSource
 
 @Repository
-class ImageRepository {
+class ImageRepository(private val dataSource: DataSource) {
     fun save(image: Image): Image {
-        return image
+        val sql = """
+            INSERT INTO item_images (image_data, file_type, alt_text)
+            VALUES (?, ?, ?)
+            RETURNING id
+        """.trimIndent()
+
+        dataSource.connection.use { conn ->
+            conn.prepareStatement(sql).use { stmt ->
+                stmt.setBytes(1, image.data)
+                stmt.setString(2, image.fileType)
+                stmt.setString(3, image.altText)
+
+                val rs = stmt.executeQuery()
+                if (rs.next()) {
+                    val id = rs.getInt("id")
+                    return image.copy(id = id)
+                } else {
+                    throw RuntimeException("Failed to save image, no ID obtained.")
+                }
+            }
+        }
     }
 }
