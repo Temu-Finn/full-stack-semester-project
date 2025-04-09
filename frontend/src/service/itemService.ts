9dbf02cef7162265d70da94b231d281d6982e442
Henrik Utistog Hausberg <henrik.utistog.hausberg@outlook.com>
2025-04-09 23:51:30 +0200

    Added working search, including text search, category search and sorting

--- frontend/src/service/itemService.ts
+++ frontend/src/service/itemService.ts
@@ -66,7 +66,7 @@ const RecommendedItemsResponseSchema = z.object({
 const SearchItemParamsSchema = z
   .object({
     searchText: z.string().optional(),
-    categoryId: z.number().int().positive().optional(),
+    categoryId: z.number().int().nonnegative().optional(),
     county: z.string().optional(),
     municipality: z.string().optional(),
     city: z.string().optional(),
@@ -75,11 +75,7 @@ const SearchItemParamsSchema = z
     maxDistanceKm: z.number().min(0).optional(),
     page: z.number().int().min(0).optional(),
     size: z.number().int().positive().optional(),
-    sort: z
-      .string()
-      .regex(/^[a-zA-Z]+,(asc|desc)$/)
-      .array()
-      .optional(),
+    sort: z.string().array().optional(),
   })
   .refine(
     (data) => {
@@ -102,12 +98,12 @@ const PageMetadataSchema = z.object({
 
 const MunicipalitySchema = z.object({
   name: z.string(),
-  count: z.number().int().positive(),
+  count: z.number().int().nonnegative(),
 })
 
 const CountySchema = z.object({
   name: z.string(),
-  count: z.number().int().positive(),
+  count: z.number().int().nonnegative(),
   municipalities: z.array(MunicipalitySchema),
 })
 
